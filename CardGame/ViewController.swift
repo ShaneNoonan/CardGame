5ef498694d2812aace878225d5c320ed17699dee
Shane Noonan <ShaneNoonan@shanes-macbook-pro.local>
2016-07-29 14:48:17 +0100

    Initial Commit

---
+++ CardGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CardGame
+//
+//  Created by Shane Noonan on 29/07/2016.
+//  Copyright © 2016 Shane Noonan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
